dc072ed41f2843693a590cfa3c478f34268b1a72
iosgaz <daniel.lidoe@gmail.com>
2020-05-03 21:11:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a18danli_assignment5/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a18danli_assignment5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
